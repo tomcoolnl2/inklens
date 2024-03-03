7fb6e7fb9239e77cf7b86ad873c7cd48b8821b9e
tomcoolnl2 <tomcoolnl@gmail.com>
2024-03-03 06:43:42 +0100

    chore: api generation

---
+++ apps/inklens-api-tags/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+	displayName: 'inklens-api-tags',
+	preset: '../../jest.preset.js',
+	testEnvironment: 'node',
+	transform: {
+		'^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+	},
+	moduleFileExtensions: ['ts', 'js', 'html'],
+	coverageDirectory: '../../coverage/apps/inklens-api-tags',
+};
